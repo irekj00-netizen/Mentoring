81fa92bb708e1ff11c0863b6c9149284055fe313
Irek <irekj00@gmail.com>
2025-12-03 15:00:12 +0100

    Pandas dokończenie

--- Pandas_tasks.py
+++ Pandas_tasks.py
@@ -90,8 +90,58 @@ def pt_17(fp):
 def pt_18(fp):
     klienci = pd.read_csv(fp, index_col=0)
     klienci["last_purchase"] = pd.to_datetime(klienci["last_purchase"], dayfirst=True)
-    klienci["days_since_last_purchase"] = (pd.Timestamp("2025-11-25") - klienci["last_purchase"]).dt.days
+    klienci["days_since_last_purchase"] = (pd.Timestamp("25/11/2025") - klienci["last_purchase"]).dt.days
     return klienci
+
+def pt_19(fp):
+    klienci = pd.read_csv(fp, index_col=0)
+    klienci["last_purchase"] = pd.to_datetime(klienci["last_purchase"], dayfirst=True)
+    ost_zakup = klienci["last_purchase"].idxmax()
+    return klienci.loc[ost_zakup]
+
+def pt_20(fp):
+    klienci = pd.read_csv(fp, index_col=0)
+    return klienci.groupby("city")["total_spend"].sum()
+
+def pt_21(fp):
+    klienci = pd.read_csv(fp, index_col=0)
+    klienci[['first_name', 'last_name']] = klienci['name'].str.split(' ', n=1, expand=True)
+    return klienci
+
+def pt_22a(fp):
+    klienci = pd.read_csv(fp, index_col=0)
+    klienci.loc[2, "city"] = None
+    klienci.loc[4, "total_spend"] = None
+    klienci.loc[1, "age"] = None
+    return klienci
+
+def pt_22(klienci):
+    return klienci.columns[klienci.isna().any()].tolist()
+
+def pt_23(klienci):
+    return klienci.isna().sum()
+
+def pt_24(klienci):
+    return klienci.isna().sum().sum() / klienci.size * 100
+
+def pt_25(klienci):
+    return klienci[klienci.isna().any(axis=1)]
+
+def pt_26(klienci):
+    return klienci.dropna()
+
+def pt_27(klienci):
+    klienci['age'] = klienci['age'].fillna(klienci['age'].mean())
+    return klienci
+
+def pt_28(klienci):
+    klienci['city'] = klienci['city'].fillna('nieznane')
+    return klienci
+
+def pt_29(klienci):
+    klienci['is_missing_city'] = ~klienci['city'].isna()
+    return klienci
+
 
 # print(pt_1(file_path))
 # print(pt_2(file_path))
@@ -109,4 +159,16 @@ def pt_18(fp):
 # print(pt_15(file_path))
 # print(pt_16(file_path))
 # print(pt_17(file_path))
-print(pt_18(file_path))
+# print(pt_18(file_path))
+# print(pt_19(file_path))
+# print(pt_20(file_path))
+# print(pt_21(file_path))
+# print(pt_22a(file_path))
+# print(pt_22(pt_22a(file_path)))
+# print(pt_23(pt_22a(file_path)))
+# print("Procent brakujących danych:",pt_24(pt_22a(file_path)))
+# print(pt_25(pt_22a(file_path)))
+# print(pt_26(pt_22a(file_path)))
+# print(pt_27(pt_22a(file_path)))
+# print(pt_28(pt_22a(file_path)))
+print(pt_29(pt_22a(file_path)))
